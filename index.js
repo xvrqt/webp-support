7c63ffa5003d78584c143588cb9b5fda06776ed4
Amy <git@xvrqt.com>
2019-02-27 19:45:56 -0800

    Changed the data url because although valid, HeadlessChrome doesn't recognize it for some reason.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /* Uses an inline webp image to test if the browser supports webp. */
 export async function supportsWebp() {
   if (!self.createImageBitmap) { return false; }
-  const data = 'data:image/webp;base64,UklGRhJXRUJQVlA4TAYvQWxvaw==';
+  const data = 'data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==';
   const blob = await fetch(data).then(r => r.blob());
   return createImageBitmap(blob).then(() => true, () => false);
 }
